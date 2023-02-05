7445f80d976b7f0f0e77cef5f0caab7239e20126
Avtandil Kurbanov <avtandilkgg@gmail.com>
2023-02-05 18:18:27 +0600

    Create detail post

--- toDo/urls.py
+++ toDo/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import Home
+from .views import Home, PostDetail
 
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='home'),
+    path('post_detail/<slug:post_slug>/', PostDetail.as_view(), name='detail')
 ]
--- toDo/views.py
+++ toDo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import ToDo
 
@@ -8,3 +8,10 @@ class Home(ListView):
     model = ToDo
     template_name = 'toDo/index.html'
     context_object_name = 'posts'
+
+
+class PostDetail(DetailView):
+    model = ToDo
+    template_name = 'toDo/detail.html'
+    context_object_name = 'post'
+    slug_url_kwarg = 'post_slug'
